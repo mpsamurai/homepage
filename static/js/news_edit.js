2c0a798b89e212e816e45cc516147be55edea2ed
井上幸信 <saishininoue@gmail.com>
2019-05-26 19:53:52 +0900

    画像選択機能途中

--- static/js/news_edit.js
+++ static/js/news_edit.js
@@ -8,13 +8,16 @@
     class Canvas {
         constructor(_canvasElement, _offScreenCanvasElement, _baseImageElement, context) {
             this.canvasElement = _canvasElement;
+            this.canvasWidth;
+            this.canvasHeight;
             this.offScreenCanvasElement = _offScreenCanvasElement;
             this.canvasContext = _canvasElement.getContext(context);
             this.offScreenCanvasContext = _offScreenCanvasElement.getContext(context);
             this.baseImageElement = _baseImageElement;
+            this.setImage();
         }
 
-        drawImage() {
+        setImage() {
             const image = new Image();
 
             image.onload = function() {
@@ -24,13 +27,26 @@
 
             image.src = this.baseImageElement.src;
 
-            this.canvasElement.setAttribute('width', image.width);
-            this.canvasElement.setAttribute('height', image.height);
-            this.offScreenCanvasElement.setAttribute('width', image.width);
-            this.offScreenCanvasElement.setAttribute('height', image.height);
+            this.canvasWidth = image.width;
+            this.canvasHeight = image.height;
+
+            this.setCanvasWidth(image);
+            this.setCanvasHeight(image);
+
 
             this.canvasContext.drawImage(image, 0, 0);
         }
+
+        setCanvasWidth(image) {
+            this.canvasElement.setAttribute('width', image.width);
+            this.offScreenCanvasElement.setAttribute('width', image.width);
+        }
+
+        setCanvasHeight(image) {
+            this.canvasElement.setAttribute('height', image.height);
+            this.offScreenCanvasElement.setAttribute('height', image.height);
+        }
+
     }
 
     class RectangleSelection {
@@ -39,8 +55,6 @@
             this.canvasElementPosition = this.canvas.canvasElement.getBoundingClientRect();
             this.elementXCoordinate = this.canvasElementPosition.left + window.pageXOffset;
             this.elementYCoordinate = this.canvasElementPosition.top + window.pageYOffset;
-            this.canvasWidth = _canvas.canvasElement.clientWidth;
-            this.canvasHeight = _canvas.canvasElement.clientHeight;
             this.xStartingPoint;
             this.yStartingPoint;
             this.xCurrentPoint;
@@ -71,12 +85,12 @@
             _this.xCurrentPoint = e.pageX - _this.canvas.canvasElement.offsetLeft;
             _this.yCurrentPoint = e.pageY - _this.canvas.canvasElement.offsetTop;
 
-            _this.xCurrentPoint = _this.judgeRectangleSelectionMaximumSizeOfX(_this.xCurrentPoint, _this.canvasWidth, _this.elementXCoordinate);
-            _this.yCurrentPoint = _this.judgeRectangleSelectionMaximumSizeOfY(_this.yCurrentPoint, _this.canvasHeight + _this.elementYCoordinate, _this.elementYCoordinate);
+            _this.xCurrentPoint = _this.judgeRectangleSelectionMaximumSizeOfX(_this.xCurrentPoint, _this.canvas.canvasWidth, _this.elementXCoordinate);
+            _this.yCurrentPoint = _this.judgeRectangleSelectionMaximumSizeOfY(_this.yCurrentPoint, _this.canvas.canvasHeight + _this.elementYCoordinate, _this.elementYCoordinate);
 
-            _this.canvas.canvasContext.clearRect(0, 0, _this.canvasWidth, _this.canvasHeight);
-            _this.canvas.offScreenCanvasContext.clearRect(0, 0, _this.canvasWidth, _this.canvasHeight);
-            _this.canvas.drawImage();
+            _this.canvas.canvasContext.clearRect(0, 0, _this.canvas.canvasWidth, _this.canvas.canvasHeight);
+            _this.canvas.offScreenCanvasContext.clearRect(0, 0, _this.canvas.canvasWidth, _this.canvas.canvasHeight);
+            _this.canvas.setImage();
             _this.canvas.canvasContext.drawImage(_this.createRectangleByOffScreen(_this.xStartingPoint, _this.yStartingPoint, _this.xCurrentPoint, _this.yCurrentPoint), 0, 0);
         }
 
@@ -107,6 +121,101 @@
     }
 
     class ImageSelection {
+        constructor(_canvas, _idImageElement) {
+            this.canvas = _canvas;
+            this.idImageElement = _idImageElement;
+        }
+
+        switchImage() {
+            const image = new Image();
+            const self = this;
+            image.src = this.canvas.baseImageElement.src;
+            this.canvas.canvasContext.drawImage(image, 0, 0);
+
+            this.idImageElement.addEventListener("change", function(e){
+
+                self.selectedFile(this);
+            },false);
+        }
+
+        selectedFile(input) {
+            const self = this;
+            const file = input.files[0];
+            const reader = new FileReader();
+            reader.onload = this.loadedFile.bind(null, self);
+            reader.readAsDataURL(file);
+        }
+
+        loadedFile(_self, e) {
+            const image = new Image();
+            image.src = e.target.result;
+
+            image.onload = () => {
+                console.log(1);
+                _self.canvas.canvasContext.clearRect(0, 0, _self.canvas.canvasWidth, _self.canvas.canvasHeight);
+
+
+                _self.canvas.setImage(image);
+
+                _self.canvas.canvasContext.drawImage(image, 0, 0);
+
+
+            }
+        }
+
+
+
+
+
+
+/*
+        switchImage() {
+
+            const image = new Image();
+            const self = this;
+            image.src = this.canvas.baseImageElement.src;
+
+            this.canvas.canvasContext.drawImage(image, 0, 0);
+
+            this.idImageElement.addEventListener("change", function(e){
+                self.selectedFile(self);
+            },false);
+
+
+            const image = new Image();
+            const self = this;
+            image.src = this.canvas.baseImageElement.src;
+            console.log(image.src);
+
+            this.ctx.drawImage(img, 0, 0);
+
+            this.changeImageBtn.addEventListener("change", function(e){
+                self.onFileSelected(this);
+            },false);
+        }
+
+        selectedFile(input) {
+            const self = this;
+
+            console.log(input.files);
+            const file = input.files[0];
+            const reader = new FileReader();
+            reader.onload = this.loadedFile.bind(null, self);
+            reader.readAsDataURL(file);
+        }
+
+        loadedFile(_self, e) {
+            const img = new Image();
+
+            img.onload = () => {
+                _self.baseCanvas.setAttribute('width', img.naturalWidth);
+                _self.baseCanvas.setAttribute('height', img.naturalHeight);
+                document.getElementById('base_canvas').getContext('2d').drawImage(img, 0, 0);
+            }
+            img.src = e.target.result;
+            console.log(img.naturalHeight);
+        }
+*/
 
     }
 
@@ -118,9 +227,20 @@
     const offScreenCanvasElement = document.createElement('canvas');
     const baseImageElement = document.getElementById('base_image');
     const canvas1 = new Canvas(canvasElement, offScreenCanvasElement, baseImageElement, '2d');
-    canvas1.drawImage();
+    const idImageElement = document.getElementById('id_image');
 
     const rectangleSelection1 = new RectangleSelection(canvas1, '2d');
     rectangleSelection1.drawRectangle();
+
+    const imageSelection1 = new ImageSelection(canvas1, idImageElement);
+
+
+    imageSelection1.switchImage();
+
+
+/*
+    imageSelection1.switchImage();
+*/
+
 
 })();
