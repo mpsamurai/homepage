47a161d4194174abdbd9099c63ea7b478696d2fa
井上幸信 <saishininoue@gmail.com>
2019-06-09 13:02:22 +0900

    画像表示機能途中

--- static/js/news_edit.js
+++ static/js/news_edit.js
@@ -5,21 +5,22 @@ window.onload = function() {
 (() => {
     'use strict';
 
-    const menuElement = document.getElementsByClassName('menu');
-    menuElement[0].style.position = 'static';
+    const menuElem = document.getElementsByClassName('menu');
+    menuElem[0].style.position = 'static';
 
-    const ImageSelector = (() => {
+    const ImgSelector = (() => {
         return class {
-            constructor(idImageElement) {
+            constructor(idImgElem) {
                 const self = this;
                 this.observerList = [];
-                this._idImageElement = idImageElement;
-                this._idImageElement.addEventListener('change', () => {
+                this._idImgElem = idImgElem;
+                this._idImgElem.addEventListener('change', () => {
                     console.log(1234);
+
+
+
                 }, false);
             }
-
-
 
             addObserver(obj) {
                 this.observerList.push(obj);
@@ -76,8 +77,6 @@ window.onload = function() {
                 this.canvasElement = _canvasElement;
             }
 
-
-
             addObserver(obj) {
                 this.observerList.push(obj);
             }
@@ -90,33 +89,90 @@ window.onload = function() {
         }
     })();
 
-    const ClippedImage = (() => {
+    const ClippedImg = (() => {
         return class {
-            constructor(canvasContainerElement, canvasElement, baseImageElement, context) {
-                this._canvasContainerElement = canvasContainerElement;
-                this._canvasElement = canvasElement;
-                this._baseImageElement = baseImageElement;
-                this._canvasContext = canvasElement.getContext(context);
+            constructor(canvasContainerElem, canvasElem, baseImgElem, context) {
+                this._canvasContainerElem = canvasContainerElem;
+                this._canvasElem = canvasElem;
+                this._baseImgElem = baseImgElem;
+                this._canvasContext = canvasElem.getContext(context);
             }
 
-            setImage(baseImageElement) {
+            setImg(baseImgElem) {
                 const img = new Image();
-                const canvasContainerWidth = canvasContainerElement.clientWidth;
+                const canvasContainerWidth = canvasContainerElem.clientWidth;
+/*
                 const baseImageWidth = baseImageElement.naturalWidth;
                 const baseImageHeight = baseImageElement.naturalHeight;
+*/
 
+                const baseImgWidth = 1680;
+                const baseImgHeight = 1050;
+
+
+                var biggerResult = Math.max(baseImgWidth, baseImgHeight);
+                var smallerResult = Math.min(baseImgWidth, baseImgHeight);
+
+                console.log('大きい方: ' + biggerResult);
+                console.log('小さい方: ' + smallerResult);
+
+                const smallNumCalcBasedOnTheRatio = smallerResult / biggerResult;
+
+
+
+
+
+
+                const aspectRatio = this.findAspectRatio(canvasContainerWidth)(baseImgWidth);
+                let width = baseImgWidth * aspectRatio;
+
+                console.log('widthは ' + width);
+
+                if(width >= 1200) {
+                    width = 1200;
+                }
+
+                console.log('widthは ' + width);
+
+                let height;
+
+                if(baseImgWidth >= baseImgHeight) {
+                    height = width * smallNumCalcBasedOnTheRatio;
+                } else if(baseImgWidth <= baseImgHeight) {
+                    height = width / smallNumCalcBasedOnTheRatio;
+                }
+
+                console.log('widthは' + width);
+                console.log('heightは' + height);
+
+                canvasElem.setAttribute('width', width);
+                canvasElem.setAttribute('height', height);
+                this._canvasContext.scale(aspectRatio, aspectRatio)
+                this._canvasContext.drawImage(baseImgElem, 0, 0);
+
+/*
                 const aspectRatio = this.findAspectRatio(canvasContainerWidth)(baseImageWidth);
+                let w = baseImageWidth * aspectRatio;
                 canvasElement.setAttribute('width', baseImageWidth * aspectRatio);
                 canvasElement.setAttribute('height', baseImageHeight * aspectRatio);
                 this._canvasContext.scale(aspectRatio, aspectRatio)
                 this._canvasContext.drawImage(baseImageElement, 0, 0);
+*/
             }
 
             findAspectRatio(containerWidth) {
-                return (imageWidth) => {
-                    return containerWidth / imageWidth;
+                return (imgWidth) => {
+                    return containerWidth / imgWidth;
                 }
             }
+
+            findGreatestCommonDivisor(x) {
+                return (y) => {
+                    if(y === 0) return x;
+                    return findGreatestCommonDivisor(y)(x % y);
+                }
+            }
+
 
             update(value) {
                 console.log('clippedImage ' + value);
@@ -124,28 +180,68 @@ window.onload = function() {
         }
     })();
 
-    const canvasContainerElement = document.getElementById('canvas_container');
-    const canvasElement = document.getElementById('canvas');
-    const offScreenCanvasElement = document.createElement('canvas');
-    const baseImageElement = document.getElementById('base_image');
-    const idImageElement = document.getElementById('id_image');
-    const slideBarOfScalingElement = document.getElementById('slide_bar_of_scaling');
+    const canvasContainerElem = document.getElementById('canvas_container');
+    const canvasElem = document.getElementById('canvas');
+    const offScreenCanvasElem = document.createElement('canvas');
+    const baseImgElem = document.getElementById('base_image');
+    const idImgElem = document.getElementById('id_image');
+    const slideBarOfScalingElem = document.getElementById('slide_bar_of_scaling');
 
     const scale1 = new Scale();
     const preview1 = new Preview();
-    const imageSelector1 = new ImageSelector(idImageElement);
-    const canvas1 = new Canvas(canvasElement);
-    const clippedImage1 = new ClippedImage(canvasContainerElement, canvasElement, baseImageElement, '2d');
+    const imgSelector1 = new ImgSelector(idImgElem);
+    const canvas1 = new Canvas(canvasElem);
+    const clippedImg1 = new ClippedImg(canvasContainerElem, canvasElem, baseImgElem, '2d');
 
-    scale1.addObserver(clippedImage1);
-    preview1.addObserver(clippedImage1);
-    imageSelector1.addObserver(clippedImage1);
-    canvas1.addObserver(clippedImage1);
+    scale1.addObserver(clippedImg1);
+    preview1.addObserver(clippedImg1);
+    imgSelector1.addObserver(clippedImg1);
+    canvas1.addObserver(clippedImg1);
 
-    clippedImage1.setImage(baseImageElement);
-
-
+    clippedImg1.setImg(baseImgElem);
 
 
+
+
+
+
+/*
+function findGreatestCommonDivisor(x, y) {
+    if(y === 0) return x;
+    return findGreatestCommonDivisor(y, x % y);
+}
+*/
+
+function findGreatestCommonDivisor(x) {
+    return (y) => {
+        if(y === 0) return x;
+        return findGreatestCommonDivisor(y)(x % y);
+    }
+}
+
+/*
+let w = 600;
+let h = 1500;
+
+const gcd = findGreatestCommonDivisor(w)(h);
+
+console.log('最大公約数: ' + gcd);
+
+let widthRatio = w / gcd;
+let heightRatio = h / gcd;
+
+console.log('横の比率: ' + widthRatio);
+console.log('縦の比率: ' + heightRatio);
+
+(function() {
+    if(widthRatio >= heightRatio) {
+        console.log('横幅: ' + w * (heightRatio / widthRatio));
+        console.log('縦幅: ' + h * (heightRatio / widthRatio));
+    } else {
+        console.log('横幅: ' + w * (widthRatio / heightRatio));
+        console.log('縦幅: ' + h * (widthRatio / heightRatio));
+    }
+})();
+*/
 
 })();
