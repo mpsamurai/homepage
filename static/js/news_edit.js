1e550c303c654982033f6a7be4d2b44c3837a758
井上幸信 <saishininoue@gmail.com>
2019-06-18 07:56:15 +0900

    矩形選択機能 途中

--- static/js/news_edit.js
+++ static/js/news_edit.js
@@ -101,9 +101,191 @@
 
     const Canvas = (() => {
         return class {
-            constructor(_canvasElement) {
+            constructor(canvasContainerElem, canvasElem, offScreenCanvasElem, context) {
                 this.observerList = [];
-                this.canvasElement = _canvasElement;
+/*
+                this._canvasElement = canvasElement;
+*/
+
+                this.canvasContainerElem = canvasContainerElem;
+                this.canvasElem = canvasElem;
+
+                this.offScreenCanvasElem = offScreenCanvasElem;
+
+                this.canvasContext = this.canvasElem.getContext(context);
+                this.offScreenCanvasContext = this.offScreenCanvasElem.getContext(context);
+                this.canvasElemPosition = this.canvasElem.getBoundingClientRect();
+                this.coordinate;
+
+                this.coordinate = {xCoordinateOfElem: this.canvasElemPosition.left + window.pageXOffset};
+                this.coordinate = {yCoordinateOfElem: this.canvasElemPosition.top + window.pageYOffset};
+                this.coordinate.xStartingPoint;
+                this.coordinate.yStartingPoint;
+                this.coordinate.xCurrentPoint;
+                this.coordinate.yCurrentPoint;
+
+
+                this.elementXCoordinate = this.canvasElemPosition.left + window.pageXOffset;
+                this.elementYCoordinate = this.canvasElemPosition.top + window.pageYOffset;
+                this.xStartingPoint;
+                this.yStartingPoint;
+                this.xCurrentPoint;
+                this.yCurrentPoint;
+            }
+
+            drawRectangle() {
+
+
+                const valAdjustRectangleSelectionSize = this.adjustRectangleSelectionSize.bind(null)(this)('test1')('test2');
+                this.startRectangleSelected(valAdjustRectangleSelectionSize)(this.canvasElem)(this.coordinate);
+//                this.endRectangleSelected(valAdjustRectangleSelectionSize);
+
+
+
+
+/*
+                const valAdjustRectangleSelectionSize = this.adjustRectangleSelectionSize.bind(null, this);
+                this.startRectangleSelected(valAdjustRectangleSelectionSize);
+                this.endRectangleSelected(valAdjustRectangleSelectionSize);
+*/
+
+            }
+
+            startRectangleSelected(valAdjustRectangleSelectionSize) {
+                return (canvasElem) => {
+                    return (coordinate) => {
+                        canvasElem.test = 'testです';
+                        canvasElem.addEventListener('mousedown', (e) => {
+                            console.log(e.target.test);
+
+
+
+                        });
+                    }
+                }
+
+
+/*
+                document.addEventListener('mousedown', (e) => {
+                    this.xStartingPoint = e.pageX - this.canvasElem.offsetLeft;
+                    this.yStartingPoint = e.pageY - this.canvasElem.offsetTop;
+                    console.log(this.xStartingPoint);
+                    console.log(this.yStartingPoint);
+                    document.addEventListener('mousemove', valAdjustRectangleSelectionSize);
+                });
+*/
+
+
+
+
+            }
+
+            endRectangleSelected(valAdjustRectangleSelectionSize) {
+                document.addEventListener('mouseup', (e) => {
+                    console.log('end');
+
+                    document.removeEventListener('mousemove', valAdjustRectangleSelectionSize);
+                });
+            }
+
+            adjustRectangleSelectionSize(self) {
+                return (test1) => {
+                    return (test2) => {
+                        return (e) => {
+/*
+                            console.log(e);
+                            console.log('***' + self);
+                            console.log('***' + test1);
+                            console.log('***' + test2);
+*/
+                        }
+                    }
+                }
+
+/*
+                self.xCurrentPoint = e.pageX - self.canvasElem.offsetLeft;
+                self.yCurrentPoint = e.pageY - self.canvasElem.offsetTop;
+*/
+
+
+
+
+
+
+
+/*
+                self.canvasContext.drawImage(self.createRectangleByOffScreen(self.xStartingPoint, self.yStartingPoint, self.xCurrentPoint, self.yCurrentPoint), 0, 0);
+*/
+
+
+
+/*
+                _this.xCurrentPoint = e.pageX - _this.canvas.offsetLeft;
+                _this.yCurrentPoint = e.pageY - _this.canvas.offsetTop;
+
+                _this.xCurrentPoint = _this.judgeRectangleSelectionMaximumSizeOfX(_this.xCurrentPoint, _this.canvas.canvasWidth, _this.elementXCoordinate);
+                _this.yCurrentPoint = _this.judgeRectangleSelectionMaximumSizeOfY(_this.yCurrentPoint, _this.canvas.canvasHeight + _this.elementYCoordinate, _this.elementYCoordinate);
+
+                _this.canvas.setImage();
+                _this.canvas.canvasContext.drawImage(_this.createRectangleByOffScreen(_this.xStartingPoint, _this.yStartingPoint, _this.xCurrentPoint, _this.yCurrentPoint), 0, 0);
+*/
+            }
+
+
+
+
+
+
+            createRectangleByOffScreen(xStartingPoint, yStartingPoint, xCurrentPoint, yCurrentPoint) {
+
+
+
+
+/*
+                this.offScreenCanvasElem.width = 10000;
+                this.offScreenCanvasElem.height = 10000;
+
+                console.log('offScreenCanvas: ' + this.offScreenCanvasElem.width);
+                console.log('offScreenCanvas: ' + this.offScreenCanvasElem.height);
+*/
+
+//                this.offScreenCanvasContext.strokeRect(xStartingPoint, yStartingPoint - this.elementYCoordinate, xCurrentPoint - xStartingPoint, yCurrentPoint - yStartingPoint);
+                this.offScreenCanvasContext.fillRect(0, 0, 100, 100);
+                return this.offScreenCanvasElem;
+            }
+
+            judgeRectangleSelectionMaximumSizeOfX(currentPoint, maximumSize, minimumSize) {
+                if(currentPoint >= maximumSize) {
+                    return maximumSize - 1;
+                } else if(currentPoint <= minimumSize) {
+                    return 1;
+                } else {
+                    return currentPoint;
+                }
+            }
+
+            judgeRectangleSelectionMaximumSizeOfY(currentPoint, maximumSize, minimumSize) {
+                if(currentPoint >= maximumSize) {
+                    return maximumSize - 1;
+                } else if(currentPoint <= this.elementYCoordinate) {
+                    return this.elementYCoordinate + 1;
+                } else {
+                    return currentPoint;
+                }
+            }
+
+
+
+
+
+
+
+            get canvasElement() {
+                return this._canvasElement;
+            }
+
+            set canvasElement(val) {
+                this._canvasElement = val;
             }
 
             addObserver(obj) {
@@ -112,7 +294,7 @@
 
             notifyObserver() {
                 for (let i = 0, len = this.observerList.length; i < len; i++ ) {
-                    this.observerList[i].update('canvas');
+                    //this.observerList[i].canvasElem = this.canvasElem;
                 }
             }
         }
@@ -120,15 +302,15 @@
 
     const ClippedImg = (() => {
         return class {
-            constructor(canvasContainerElem, canvasElem, baseImgElem, imgElem, scaleObj, context) {
+            constructor(baseImgElem, imgElem, scaleObj, canvasObj, context) {
                 const self = this;
-                this._canvasContainerWidth = canvasContainerElem.clientWidth;
-                this._canvasElem = canvasElem;
                 this._baseImgElem = baseImgElem;
                 this._imgElem = imgElem;
                 this._imgSize;
                 this._scaleObj = scaleObj;
-                this._canvasContext = canvasElem.getContext(context);
+                this._canvasContainerWidth = canvasObj.canvasContainerElem.clientWidth;
+                this._canvasElem = canvasObj.canvasElem;
+                this._canvasContext = canvasObj.canvasContext;
                 this.imgElem.src = baseImgElem.src;
 
                 this.imgElem.onload = () => {
@@ -234,7 +416,6 @@
         }
     })();
 
-    const bodyElem = document.getElementsByTagName('body');
     const canvasContainerElem = document.getElementById('canvas_container');
     const canvasElem = document.getElementById('canvas');
     const offScreenCanvasElem = document.createElement('canvas');
@@ -246,12 +427,13 @@
     const scale1 = new Scale(slideBarOfScalingElem);
     const preview1 = new Preview();
     const imgSelector1 = new ImgSelector(idImgElem);
-    const canvas1 = new Canvas(canvasElem);
-    const clippedImg1 = new ClippedImg(canvasContainerElem, canvasElem, baseImgElem, imgElem, scale1, '2d');
+    const canvas1 = new Canvas(canvasContainerElem, canvasElem, offScreenCanvasElem, '2d');
+    const clippedImg1 = new ClippedImg(baseImgElem, imgElem, scale1, canvas1, '2d');
 
     scale1.addObserver(clippedImg1);
     preview1.addObserver(clippedImg1);
     imgSelector1.addObserver(clippedImg1);
     canvas1.addObserver(clippedImg1);
 
+    canvas1.drawRectangle();
 })();
