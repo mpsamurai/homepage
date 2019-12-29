3cd8e0ab8f9deed02df3836ed326dbd9e85e9639
井上幸信 <saishininoue@gmail.com>
2019-12-29 15:00:21 +0900

    s3設定 環境変数の設定

--- homepage/settings/base.py
+++ homepage/settings/base.py
@@ -90,18 +90,18 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
+AWS_DEFAULT_ACL = None
+
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazon.com' % (AWS_STORAGE_BUCKET_NAME)
 AWS_S3_OBJECT_PARAMETERS = {
-    'CacheControl': 'max-age=86400',  # 1日はそのキャッシュを使う
+    'CacheControl': 'max-age=86400',
 }
 
-# 静的ファイルの設定
 STATICFILES_STORAGE = 'homepage.settings.backends.StaticFilesStorage'
 
-# メディアファイルの設定。今回は「project」というプロジェクト名の例
 DEFAULT_FILE_STORAGE = 'homepage.settings.backends.MediaStorage'
 
 AWS_S3_STATIC_ROOT = 'static'
--- homepage/settings/develop.py
+++ homepage/settings/develop.py
@@ -9,7 +9,3 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
-AWS_ACCESS_KEY_ID = ''
-AWS_SECRET_ACCESS_KEY = ''
-AWS_STORAGE_BUCKET_NAME = ''
